10530ee89b749ec0480bc38b01a317013b6d7b14
KAMILA <dudekkamila@icloud.com>
2023-12-04 23:14:38 +0100

    added component About

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Build_a_Portfolio_Using_React![Screen Shot 2023-12-04 at 22.55.19.png](..%2F..%2F..%2F..%2Fvar%2Ffolders%2F6n%2Flcts__610tq1zsf6mqnhgkx00000gq%2FT%2FTemporaryItems%2FNSIRD_screencaptureui_3mg9uL%2FScreen%20Shot%202023-12-04%20at%2022.55.19.png)
+# Build_a_Portfolio_Using_React
 
 # Technologies
 CSS
---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import AvatarCopy from '../assets/AvatarCopy.JPG';
+
+
+const About = () => {
+    return (
+        <div id="about" className="about">
+            <h1 className="about-heading">About Me</h1>
+            <div className="about-info">
+                <p className="about-desc">
+
+                    A dedicated and hardworking Junior Frontend Developer transitioning from the logistics
+                    industry to IT. I bring strong analytical skills and a passion for problem-solving, driving me to continually improve in the programming realm. Always eager for new challenges, I find joy in exploring cutting-edge technologies and creating innovative solutions.
+                    My experience in the logistics sector has instilled in me flexibility,
+                    quick decision-making, and effective communication – skills that
+                    I now transfer to the field of Frontend Development.
+                    My mission is to thrive in the dynamic IT environment, invest effort in crafting
+                    intuitive and aesthetically pleasing user interfaces, and deliver value through an
+                    innovative approach to projects.
+                </p>
+                <div className="about-img">
+                    <div className="about-img-wrapper">
+                        <img src={AvatarCopy} alt="Kamila Garbowska foto" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
