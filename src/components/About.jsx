79af93880d6edaa175661436da632ee9270d2192
KAMILA <dudekkamila@icloud.com>
2024-02-23 14:44:09 +0100

    change name of images in components Body

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import AvatarCopy from '../assets/AvatarCopy.jpg';
+import Avatar from '../assets/Avatar.jpg';
 import './About.scss'
 
 const About = () => {
@@ -19,7 +19,7 @@ const About = () => {
                 </p>
                 <div className="about-img">
                     <div className="about-img-wrapper">
-                        <img src={AvatarCopy} alt="Kamila Garbowska foto"/>
+                        <img src={Avatar} alt="Kamila Garbowska foto"/>
                     </div>
                 </div>
             </div>
