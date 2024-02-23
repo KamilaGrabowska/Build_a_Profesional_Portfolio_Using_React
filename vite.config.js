aa81bdc61de79c3f05c610a79d184dac340b001f
KAMILA <dudekkamila@icloud.com>
2024-02-23 15:29:38 +0100

    modified file vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['src/assets/Avatar.jpg']
+      external: ['../assets/Avatar.jpg']
     }
   }
 })
